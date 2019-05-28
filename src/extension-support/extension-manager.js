3306a8a2eaf2eb7162498eae139eacdccc2cd551
Ian Honohan <ihonohan@vernier.com>
2019-05-28 10:21:48 -0700

    Add experimental GDX-MD extension

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -24,7 +24,8 @@ const builtinExtensions = {
     makeymakey: () => require('../extensions/scratch3_makeymakey'),
     boost: () => require('../extensions/scratch3_boost'),
     gdxfor: () => require('../extensions/scratch3_gdx_for'),
-    gdxsnd: () => require('../extensions/scratch3_gdx_snd')
+    gdxsnd: () => require('../extensions/scratch3_gdx_snd'),
+    gdxmd: () => require('../extensions/scratch3_gdx_md')
 };
 
 /**
